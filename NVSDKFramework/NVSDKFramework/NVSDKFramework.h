5c7f6b5a4520ce4b7010c8a374dc728f06a97eb1
macrovideo <macrovideo2018@163.com>
2018-11-05 19:27:38 +0800

    add files

---
+++ NVSDKFramework/NVSDKFramework/NVSDKFramework.h
@@ -0,0 +1,19 @@
+//
+//  NVSDKFramework.h
+//  NVSDKFramework
+//
+//  Created by Macrovideo on 2018/11/5.
+//  Copyright © 2018 Macrovideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVSDKFramework.
+FOUNDATION_EXPORT double NVSDKFrameworkVersionNumber;
+
+//! Project version string for NVSDKFramework.
+FOUNDATION_EXPORT const unsigned char NVSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVSDKFramework/PublicHeader.h>
+
+
